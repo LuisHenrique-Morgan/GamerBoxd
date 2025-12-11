cd7d3fdfe44739afd69c2eda6e1476c80118d63f
devricardorodrigues <ricardoprdecarvalho@gmail.com>
2025-12-11 16:19:15 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,8 @@
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
+const filterButtons = document.querySelectorAll('.filter-btn');
 
-let currentSlide = 0;
-
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider() {
-  slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
-
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
